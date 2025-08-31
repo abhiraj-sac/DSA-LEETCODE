5e909dfea09e7cc102d98ca705b33467e08a6baf
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2025-08-31 11:51:51 +0530

    Time: 7 ms (29.03%) | Memory: 46.3 MB (35.68%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,22 +1,21 @@
 class Solution {
     public int singleNumber(int[] arr) {
-        int n = arr.length;
+        int n=arr.length;
         Arrays.sort(arr);
-
-        if (n == 1) return arr[0];
-
-        // first element unique?
-        if (arr[0] != arr[1]) return arr[0];
-
-        // last element unique?
-        if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
-
-        // middle elements
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1]) { // AND, not OR
+        if(n==1){
+            return arr[0];
+        }
+        if(n > 1 && arr[0] != arr[1]){
+            return arr[0];
+        }
+        if(n > 1 && arr[n-1] != arr[n-2]){
+            return arr[n-1];
+        }
+        for(int i=1;i<n-1;i++){
+            if(arr[i-1] != arr[i] && arr[i+1] != arr[i]){
                 return arr[i];
             }
         }
-        return -1; // ideally kabhi yaha nahi aayega
+      return -1;
     }
 }

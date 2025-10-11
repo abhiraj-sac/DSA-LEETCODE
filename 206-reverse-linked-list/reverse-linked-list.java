698e630d0af208509945c519f4e07dbba9bfe39d
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2025-10-11 18:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (93.36%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,24 +10,37 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode one = head;
-        ListNode two = head;int c=0;
+        // ListNode one = head;
+        // ListNode two = head;int c=0;
        
-        ListNode dummy = new ListNode(0);
-        ListNode ans = dummy;
-        while(one != null){
-            one = one.next;
-            c++;
+        // ListNode dummy = new ListNode(0);
+        // ListNode ans = dummy;
+        // while(one != null){
+        //     one = one.next;
+        //     c++;
+        // }
+        // int[] arr = new int[c];int i=0;
+        // while(two != null){
+        //     arr[i++] = two.val;
+        //     two =two.next;
+        // }
+        // for(int j=arr.length-1;j>=0;j--){
+        //         ans.next = new ListNode(arr[j]);
+        //         ans=ans.next;
+        // }
+        // return dummy.next;
+
+
+        ListNode prev =null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr =next;
         }
-        int[] arr = new int[c];int i=0;
-        while(two != null){
-            arr[i++] = two.val;
-            two =two.next;
-        }
-        for(int j=arr.length-1;j>=0;j--){
-                ans.next = new ListNode(arr[j]);
-                ans=ans.next;
-        }
-        return dummy.next;
+         return prev;
+
     }
 }

eb0850d86292f482a1828d7acdb4ae6c2c54c8bc
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2025-11-23 01:05:07 +0530

    Time: 9 ms (8.84%) | Memory: 47.6 MB (5.74%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+        if (  arr[i] > 0 || st.isEmpty()) {
+            st.push(arr[i]);
+        
+        }
+        
+        else {
+
+//        else if(arr[i] > st.peek()){st.push(arr[i]);}
+                while (!st.isEmpty() && st.peek() + arr[i] < 0 && st.peek() > 0) {
+                    st.pop();
+                }
+            if (st.isEmpty() || st.peek() < 0) {
+                st.push(arr[i]);
+            }
+            else if (st.peek() + arr[i] == 0) {
+                    // both destroyed
+                    st.pop();
+                }
+            
+        }
+    }
+       List<Integer> rems = new ArrayList<>();
+        while (!st.isEmpty()) {
+            rems.add(st.pop());
+        }
+
+        Collections.reverse(rems);
+
+        return rems.stream().mapToInt(i -> i).toArray();
+
+}
+}

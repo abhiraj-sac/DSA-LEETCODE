327cf00a59f8cea1fe03b11f7449b19f81566ef0
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2026-01-06 11:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (5.46%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,25 +13,43 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null){return ans;}
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int n = q.size();
+//             for(int i=0;i<n;i++){
+//                 TreeNode curr = q.poll();
+//                 if(i == n-1){
+//                 ans.add(curr.val);
+//             }
+//             if(curr.left != null){q.offer(curr.left);}
+//             if(curr.right != null){q.offer(curr.right);}
+            
+//             }
+            
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null){return ans;}
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int n = q.size();
-            for(int i=0;i<n;i++){
-                TreeNode curr = q.poll();
-                if(i == n-1){
-                ans.add(curr.val);
-            }
-            if(curr.left != null){q.offer(curr.left);}
-            if(curr.right != null){q.offer(curr.right);}
-            
-            }
-            
-        }
-        return ans;
+        List<Integer> lists  = new ArrayList<>();
+        int currdepth=0;
+        helper(root,lists,currdepth);
+        return lists;
+    }
+    public void helper(TreeNode root, List<Integer> lists,int currdepth){
+       if(root == null){return;}
+       if(currdepth == lists.size()){
+        lists.add(root.val);
+       }
+       helper(root.right,lists,currdepth+1);
+       helper(root.left,lists,currdepth+1);
     }
 }

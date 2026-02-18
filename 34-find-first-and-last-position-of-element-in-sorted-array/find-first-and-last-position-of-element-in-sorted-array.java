c49ab03865dcc0c39f146dfccf0bcf55580ce649
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2026-02-18 22:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (71.29%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,37 @@
 class Solution {
-    static  int lower(int[] arr,int t){
-        int l=0;int h=arr.length-1;int mid=0;int ans=arr.length;
+    static int lower(int[] arr,int t){
+        int l=0;int h=arr.length-1;int ans=arr.length;
         while(l <= h){
-            mid = (l+h)/2;
-            if(arr[mid] >= t){
-                ans = mid;
-                h=mid-1;
-            }
-            else{
-                l =mid+1;
+            int mid = (l+h)/2;
+            if (arr[mid] >= t) {
+                h = mid-1;
+            }else{
+                l=mid+1;
             }
         }
-        return ans;
+        return l;
     }
-    static  int upper(int[] arr,int t){
-        int l=0;int h=arr.length-1;int mid=0;int ans=arr.length;
+
+    static int upper(int[] arr,int t){
+        int l=0;int h=arr.length-1;int ans=arr.length;
         while(l <= h){
-            mid = (l+h)/2;
-            if(arr[mid] > t){
-                ans = mid;
-                h=mid-1;
-            }
-            else{
-                l =mid+1;
+            int mid = (l+h)/2;
+            if (arr[mid] > t) {
+                h = mid-1;
+            }else{
+                l=mid+1;
             }
         }
-        return ans;
+        return h;
     }
+   
     public int[] searchRange(int[] arr, int tar){
-        int l = lower(arr,tar);
-        int u =upper(arr,tar);
-        int[] ans = new int[2];
-        if(l == arr.length || arr[l] != tar){
-            return new int[]{-1, -1};
-        }
-        return new int[]{l,u-1}; 
+        int[] ans =new int[2];
+        ans[0] = -1;ans[1] = -1;
+      int l=  lower(arr,tar);
+       int h = upper(arr,tar);
+       if(l == arr.length || arr[l] != tar){return ans;}
+       ans[0] = l;ans[1] = h;
+       return ans;
     }
 }

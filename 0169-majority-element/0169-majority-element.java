f1a3761c6e76cf505fb5e5d8211a5e27fc6e0c8d
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2026-08-11 14:32:57 +0530

    Time: 1 ms (99.88%), Space: 55.7 MB (46.88%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int c=0;int ele=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(c==0){
+        //         ele = arr[i];
+        //         c=1;
+        //     }
+        //     else if(arr[i] == ele){c++;}
+        //     else{c--;}
+        // }
+        // return ele;
+        for(int i=0;i<arr.length;i++){
+            if(c==0){
+                ele=arr[i];
+                c=1;
+            }
+            else if(arr[i] == ele){c++;}
+            else{c--;}
+        }
+        return ele;
+    }
+}

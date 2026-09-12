422ca87280132851a207bd37c56e3041cc486d11
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2026-09-12 20:58:05 +0530

    Time: 4 ms (2.81%) | Memory: 43.5 MB (12.34%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,33 +3,21 @@ class Solution {
         return IntStream.range(0, array.length - 1).allMatch(i -> array[i] <= array[i + 1]);
     }
     public boolean check(int[] arr) {
-        int point=0;
+        int p=0;int n=arr.length;
         if(isSorted(arr)){return true;}
-        int n =arr.length;
-        // if(n >= 2 && arr[n-1] < arr[n-2]){}
-
-        for(int i=1;i<n;i++){
-            if( arr[i] < arr[i-1]){
-                point = i;
+        for(int i=0;i<n-1;i++){
+            if(arr[i] > arr[i+1]){
+                p =i+1;
                 break;
             }
         }
-        int[] a = new int[n];
-    int k = 0;
-
-    // point ke baad wale elements daalo
-    for (int i = point; i < n; i++) {
-        a[k++] = arr[i];
-    }
-
-    // fir point se pehle wale daalo
-    for (int i = 0; i < point; i++) {
-        a[k++] = arr[i];
-    }
-        if(isSorted(a)){
-            return true;
+        int[] ans = new int[n];int j=0;
+        for(int i=p;i<n;i++){
+        ans[j++] = arr[i];
+        }       
+        for(int i=0;i<p;i++){
+            ans[j++] = arr[i];
         }
-        return false;
+        return isSorted(ans);
     }
-
 }

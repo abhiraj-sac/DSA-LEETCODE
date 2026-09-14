7c27c4995288796ea7dd05115ae4e629bd3cdc10
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2026-09-14 21:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (66.66%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int searchInsert(int[] arr, int target) {
-        int l=0;int h=arr.length-1;int ans=arr.length;int flag=0;
+    public int searchInsert(int[] arr, int t) {
+        int l=0;int h=arr.length-1;int ans=arr.length;
         while(l <= h){
             int mid = (l+h)/2;
-            if(arr[mid] == target){ans=mid;flag =1;break;}
-            else if(arr[mid] > target){
-                ans = mid;
-                h=mid-1;
+            if(arr[mid] == t){return mid;}
+           else if(arr[mid] > t){
+              ans = mid;
+              h = mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        if(flag == 1){return ans;}
         return ans;
     }
 }
